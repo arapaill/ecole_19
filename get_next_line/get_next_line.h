2a4266c660532adc2258f511111c483d4a741f71
Alexandre Rapaille <arapaill@e-r5-p5.s19.be>
2019-11-06 16:49:07 +0100

    libft et gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arapaill <arapaill@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/05 11:39:34 by arapaill          #+#    #+#             */
+/*   Updated: 2019/11/06 10:32:51 by arapaill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int get_next_line(int fd, char **line)
+
+#endif
